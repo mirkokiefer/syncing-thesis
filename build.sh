7f263214ffd49c389cc2f56df370b1b5bba92b53
Mirko Kiefer <mail@mirkokiefer.com>
2013-04-03 21:52:33 +0200

    update build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+cd latex
+sh build.sh
+mv bsc-thesis.pdf ../thesis.pdf
--- latex/build.sh
+++ latex/build.sh
@@ -3,4 +3,3 @@ pdflatex bsc-thesis.tex
 bibtex bsc-thesis
 pdflatex bsc-thesis.tex
 pdflatex bsc-thesis.tex
-mv bsc-thesis.pdf ../thesis.pdf
